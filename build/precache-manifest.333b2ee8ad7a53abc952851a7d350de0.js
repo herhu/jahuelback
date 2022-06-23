a3d1f0845dced8bb436c164f2f82b0b169f5e983
herhu <hernanhumglz@gmail.com>
2022-06-23 14:09:36 +0200

    back

---
+++ build/precache-manifest.333b2ee8ad7a53abc952851a7d350de0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2cf63bef58d49cec6c",
+    "url": "backoffice/static/css/main.b613c0ea.chunk.css"
+  },
+  {
+    "revision": "2f2cf63bef58d49cec6c",
+    "url": "backoffice/static/js/main.d3d09d99.chunk.js"
+  },
+  {
+    "revision": "a07b140e0e84e6570c7a",
+    "url": "backoffice/static/js/runtime~main.9a99149e.js"
+  },
+  {
+    "revision": "fe4720bf89d86c33f94b",
+    "url": "backoffice/static/css/2.6a4732a9.chunk.css"
+  },
+  {
+    "revision": "fe4720bf89d86c33f94b",
+    "url": "backoffice/static/js/2.fe364e93.chunk.js"
+  },
+  {
+    "revision": "7d30878a44fe8b120091",
+    "url": "backoffice/static/js/3.aac6d211.chunk.js"
+  },
+  {
+    "revision": "83567f3ebd0ba215800ddeb7c1841e67",
+    "url": "backoffice/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d84256fae1f1a5dc7cd30389486c395c.js"
+  "backoffice/precache-manifest.333b2ee8ad7a53abc952851a7d350de0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("backoffice/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
