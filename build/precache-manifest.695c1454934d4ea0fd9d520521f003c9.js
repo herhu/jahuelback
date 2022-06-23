100123b348c0be870baef88d982782cdd0b09c1f
herhu <hernanhumglz@gmail.com>
2022-06-23 20:05:52 +0200

    l

---
+++ build/precache-manifest.695c1454934d4ea0fd9d520521f003c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fa5a229ca3f63cfa896",
+    "url": "/static/css/main.b613c0ea.chunk.css"
+  },
+  {
+    "revision": "5fa5a229ca3f63cfa896",
+    "url": "/static/js/main.94f6ca1d.chunk.js"
+  },
+  {
+    "revision": "4d8c1c3b75c20ebd45c1",
+    "url": "/static/js/runtime~main.f3c95d74.js"
+  },
+  {
+    "revision": "e4f06d73c979c48ae624",
+    "url": "/static/css/2.6a4732a9.chunk.css"
+  },
+  {
+    "revision": "e4f06d73c979c48ae624",
+    "url": "/static/js/2.fa32363c.chunk.js"
+  },
+  {
+    "revision": "7d30878a44fe8b120091",
+    "url": "/static/js/3.aac6d211.chunk.js"
+  },
+  {
+    "revision": "e6a7a0ff9bcc567f109c9e896e868544",
+    "url": "/index.html"
+  }
+];
--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { IInventoryHotel } from '../../interfaces/IInventory'
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://pagos.jahuel.cl/v1'
 });
 
 
